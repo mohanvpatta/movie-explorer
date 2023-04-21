d44b9e1b96cac4376f955916d714efa2f0cf5ff0
mohanvpatta <mohanvpatta@gmail.com>
2023-04-21 06:19:35 +0530

    fc

--- pages/index.js
+++ pages/index.js
@@ -1,124 +1,125 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import React, { useState, useEffect } from "react";
+import Feed from "@/components/Feed";
+import CustomSlider from "@/components/CustomSlider";
+import Image from "next/image";
 
 export default function Home() {
+  const [users, setUsers] = useState([26, 53, 252, 227]);
+  const [selectedMovieId, setSelectedMovieId] = useState(64);
+  const [selectedMovieThumbnails, setSelectedMovieThumbnails] = useState([]);
+  const [selectedMovie, setSelectedMovie] = useState(null);
+  const userOptions = [
+    26, 37, 49, 53, 74, 120, 136, 139, 154, 157, 159, 171, 176, 192, 217, 227,
+    252, 265,
+  ];
+
+  const changeUser = (user, index) => {
+    const newUsers = [...users];
+    newUsers[index] = user;
+    setUsers(newUsers);
+  };
+
+  const changeSelectedMovie = (movie) => {
+    setSelectedMovie(movie);
+  };
+
+  useEffect(() => {
+    fetch(`https://thumbnails.up.railway.app/tmdb/${selectedMovieId}`)
+      .then((res) => res.json())
+      .then((data) => {
+        fetch(
+          `https://api.themoviedb.org/3/movie/${data}?api_key=7a1009b4e49581207130211394ef3d89`
+        )
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            setSelectedMovie(data);
+          });
+      });
+  }, [selectedMovieId]);
+
+  useEffect(() => {
+    fetch(`https://thumbnails.up.railway.app/custom/${selectedMovieId}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setSelectedMovieThumbnails(data);
+        console.log(data);
+      });
+  }, [selectedMovieId]);
+
+  const changeSelectedMovieId = (id) => {
+    setSelectedMovieId(id);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <div className="flex max-w-fit mx-auto my-16 gap-16">
+      <div className="max-w-[800px]">
+        {users.map((user, index) => (
+          <div className="flex gap-4 flex-col items-center p-4" key={user}>
+            <Feed
+              user={user}
+              index={index}
+              changeUser={changeUser}
+              changeSelectedMovieId={changeSelectedMovieId}
+              userOptions={userOptions}
             />
-          </a>
+          </div>
+        ))}
+      </div>
+      {selectedMovie && (
+        <div className="max-w-[320px] basis-full py-4">
+          <div className="flex justify-between py-2 mb-4 font-medium">
+            <div className="text-neutral-200">{selectedMovie.title}</div>
+            <div className="text-neutral-500">
+              {selectedMovie.release_date.split("-")[0]}
+            </div>
+          </div>
+          <div>
+            <Image
+              src={`https://raw.githubusercontent.com/mohanvpatta/movie-posters/main/${selectedMovieId}.jpg`}
+              alt="id"
+              width={800}
+              height={800}
+              style={{
+                width: "100%",
+                height: "auto",
+              }}
+            />
+          </div>
+          <div>
+            <div className="my-4 mb-6 ">
+              <div className="my-4 font-medium text-neutral-500">Overview</div>
+              <div className="text-neutral-300">
+                {`${selectedMovie.overview.substring(0, 200)}... `}
+                <a
+                  href={`https://www.themoviedb.org/movie/${selectedMovie.id}`}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="underline text-neutral-500"
+                >
+                  {"Read more"}
+                </a>
+              </div>
+            </div>
+            <div className="my-4 mb-6 ">
+              <div className="my-4 font-medium text-neutral-500">Genres</div>
+              <div className="text-neutral-300">
+                {selectedMovie.genres.map((genre) => genre.name).join(", ")}
+              </div>
+            </div>
+            {selectedMovieThumbnails.length > 0 && (
+              <div className="my-4 mb-6 ">
+                <div className="my-4 font-medium text-neutral-500">
+                  Custom Thumbnails
+                </div>
+                <div className="w-full overflow-hidden">
+                  <CustomSlider thumbnails={selectedMovieThumbnails} />
+                </div>
+              </div>
+            )}
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      )}
+    </div>
+  );
 }
